6f132295e137cc1755d6693f506e233cffdb68d9
abhinav srivastav <abhinavsriva@gmail.com>
2025-12-23 00:18:59 +0100

    path corrected

--- src/traceml/tracer.py
+++ src/traceml/tracer.py
@@ -48,7 +48,7 @@ def main():
         exit_code = 1
     finally:
         tracker.stop()
-        tracker.log_summaries(log_file_path="traceml_system_summary.txt")
+        tracker.log_summaries(path="traceml_system_summary.txt")
 
     if error:
         print("\n--- script crashed here ---", file=sys.stderr)
