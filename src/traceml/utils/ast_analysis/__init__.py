57f9b16aeb37c3ca22a31a2e0347a639aebd8ea7
ppraneth <pranethparuchuri@gmail.com>
2026-03-26 14:26:15 +0530

    refactor: remove dead param_estimator.py and model_registry.py from ast_analysis
    Signed-off-by: ppraneth <pranethparuchuri@gmail.com>

--- src/traceml/utils/ast_analysis/__init__.py
+++ src/traceml/utils/ast_analysis/__init__.py
@@ -3,22 +3,12 @@
 Provides static analysis of Python training scripts without executing them.
 
 Usage:
-    from traceml.utils.ast_analysis import analyze_script, estimate_params, scan_for_optimizer
+    from traceml.utils.ast_analysis import analyze_script, scan_for_optimizer
 
     findings = analyze_script("train.py")
-    estimate = estimate_params("train.py", findings)
     optimizer = scan_for_optimizer("train.py")
 """
 
-from traceml.utils.ast_analysis.model_registry import (
-    ModelSpec,
-    list_known_models,
-    lookup_model,
-)
-from traceml.utils.ast_analysis.param_estimator import (
-    ParamEstimate,
-    estimate_params,
-)
 from traceml.utils.ast_analysis.scanner import (
     CodeFindings,
     DataLoaderFinding,
@@ -38,7 +28,6 @@ __all__ = [
     "analyze_script",
     "detect_strategy_hint",
     "scan_for_optimizer",
-    "estimate_params",
     # Data classes
     "CodeFindings",
     "DataLoaderFinding",
@@ -47,10 +36,5 @@ __all__ = [
     "ModelFinding",
     "OptimizerFinding",
     "PrecisionFinding",
-    "ParamEstimate",
-    "ModelSpec",
     "ScriptLocation",
-    # Registry helpers
-    "lookup_model",
-    "list_known_models",
 ]
